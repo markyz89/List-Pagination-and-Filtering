9561385ed747b6358ca85e37ee6e2160f8470eda
Mark Simpson <msimpson@VEBU.local>
2020-01-31 18:58:14 +0100

    project complete?

--- js/script.js
+++ js/script.js
@@ -7,35 +7,47 @@ FSJS project 2 - List Filter and Pagination
 
 
 /*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
+Defining my global variables
 ***/
 
-
+// Selecting all students on the page
+const studentListItems = document.querySelectorAll('.student-item');
+// Hardcoding a value for number of students per page
+const studentsPerPage = 10;
 
 
 /*** 
    Create the `showPage` function to hide all of the items in the 
    list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
 ***/
 
+const showPage = (list, page) => {
+   // determining start and end points for the list. It's called from within the event listener below
+   // and the page parameter is populated by the number clicked on by the user.
+   let startIndex = (page * studentsPerPage ) - studentsPerPage;
+   let endIndex = page * studentsPerPage;
 
+   // console was throwing an error because increments of 10
+   // this adjustment to endIndex reduces the endindex to the number of list items
+   // if endIndex is greater than the number of list items
+   if (endIndex > list.length) {
+      endIndex = list.length; 
+   }
+
+   // console.log(startIndex, endIndex);
+
+
+   let studentList = document.querySelector('.student-list');
+   // empty the div
+   studentList.innerHTML = '';
+
+   // fill the div with appropriate number of students.
+   for (let i=startIndex; i < endIndex; i++ ) {
+      // list parameter passed through two functions finally used here.
+      studentList.appendChild(list[i]);
+   }
+
+}
 
 
 /*** 
@@ -43,8 +55,64 @@ FSJS project 2 - List Filter and Pagination
    functionality to the pagination buttons.
 ***/
 
+const appendPageLinks = (list) => {
 
+   // take list parameter and divide by items per page and rounded up to figure out how many pages
+   let numberOfPages = Math.ceil(list.length / studentsPerPage);
+   let divPage = document.querySelector('.page');
 
+   // creating the HTML elements - div > ul
+   let pagination = document.createElement('DIV');
+   pagination.className = "pagination";
+   paginationList = document.createElement('UL');
+   pagination.appendChild(paginationList);
+   // and adding them to the appropriate place in the DOM
+   divPage.appendChild(pagination);
 
+   createListItems();
+
+   // calling the show page function to show the first page. This can be changed with the event listener below.
+   showPage(studentListItems, 1);
+
+   // Finally run the listener which allows the user to navigate between pages.
+   listenForPageNavigation();
+   
+   // these two functions are function declarations rather than function expressions with arrow functions because
+   // I got an error of "cannot access before initialization" when I made these arrow functions
+   // so I went back to good old dependable function declarations.
+   function createListItems() {
+      // adding each li in the page to the ul.
+      for (let i=1; i <= numberOfPages; i++) {
+         let item = document.createElement('LI');
+         item.innerHTML=`<a href="#">${i}</a>`;
+         if (i === 1) {
+            item.className = "active"
+         }
+         //adding my 
+         paginationList.appendChild(item);
+      }
+   }
+
+   function listenForPageNavigation () { 
+      pagination.addEventListener('click', (e) => {
+         if (e.target.tagName === "A") {
+            resetPages();
+            e.target.parentNode.className = "active";
+            let page = e.target.textContent
+            showPage(studentListItems, page);
+         }
+      })
+   }
+
+   // this removes all the active classes before it is then added to the li that was clicked.
+   const resetPages = () => {
+      for (let i=0; i < numberOfPages; i++) {
+         paginationList.children[i].className = '';
+      }
+   }
+   
+}
+
+appendPageLinks(studentListItems);
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.
